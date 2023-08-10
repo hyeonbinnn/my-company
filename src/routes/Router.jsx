3e183b043462d2a7cb8fb09fd67328823925e711
hyeonbin jeong <duafuuu@gmail.com>
2023-08-10 22:20:16 +0900

    reomve: splash 페이지 제거

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -6,20 +6,18 @@ import Services from '../pages/ServicesPage/Services';
 import Contact from '../pages/ContactPage/Contact';
 import Recruit from '../pages/RecruitPage/Recruit';
 import Error404 from '../pages/ErrorPage/Error404';
-import Splash from '../pages/SplashPage/Splash';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="*" element={<Error404 />} />
-        <Route path="/notfound" element={<Error404 />} />
-        <Route path="/splash" element={<Splash />} />
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/recruit" element={<Recruit />} />
+        <Route path="*" element={<Error404 />} />
+        <Route path="/notfound" element={<Error404 />} />
       </Routes>
     </BrowserRouter>
   );
