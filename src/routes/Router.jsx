919d34261cd404637b4e4fe2e464b0487ed457ae
hyeonbin jeong <duafuuu@gmail.com>
2023-08-11 03:35:58 +0900

    fix: 라우터 변경

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -5,19 +5,19 @@ import About from '../pages/AboutPage/About';
 import Services from '../pages/ServicesPage/Services';
 import Contact from '../pages/ContactPage/Contact';
 import Recruit from '../pages/RecruitPage/Recruit';
-import Error404 from '../pages/ErrorPage/Error404';
+// import Error404 from '../pages/ErrorPage/Error404';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/services" element={<Services />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/recruit" element={<Recruit />} />
-        <Route path="*" element={<Error404 />} />
-        <Route path="/notfound" element={<Error404 />} />
+        {/* <Route path="*" element={<Error404 />} />
+        <Route path="/notfound" element={<Error404 />} /> */}
       </Routes>
     </BrowserRouter>
   );
