6a83cb2cd613e8ec19b3f2f25215076790e2f144
hyeonbin jeong <duafuuu@gmail.com>
2023-08-11 03:39:01 +0900

    fix: 주석 제거

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,16 +4,6 @@ import companyLogo from '../../assets/companyLogo.png';
 import * as S from './Footer.style';
 
 const Footer = () => {
-  // const socialLinks = [
-  //   { url: 'https://www.youtube.com/watch?v=your-video-id', label: '유튜브' },
-  //   { url: 'https://ko-kr.facebook.com/', label: '페이스북' },
-  //   { url: 'https://www.instagram.com/', label: '인스타그램' },
-  //   {
-  //     url: 'https://section.blog.naver.com/BlogHome.naver?directoryNo=0&currentPage=1&groupId=0',
-  //     label: '블로그',
-  //   },
-  // ];
-
   return (
     <S.Footer>
       <S.FooterBg />
@@ -25,22 +15,38 @@ const Footer = () => {
         </S.H2>
         <S.SocialLink>
           <li>
-            <S.BlogLink href target="_blank" rel="noopener noreferrer">
+            <S.BlogLink
+              href="https://section.blog.naver.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <span className="a11y-hidden"> 블로그</span>
             </S.BlogLink>
           </li>
           <li>
-            <S.InstaLink href target="_blank" rel="noopener noreferrer">
+            <S.InstaLink
+              href="https://www.instagram.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <span className="a11y-hidden">인스타그램</span>
             </S.InstaLink>
           </li>
           <li>
-            <S.FacebookLink href target="_blank" rel="noopener noreferrer">
+            <S.FacebookLink
+              href="https://ko-kr.facebook.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <span className="a11y-hidden">페이스북</span>
             </S.FacebookLink>
           </li>
           <li>
-            <S.YoutubeLink href target="_blank" rel="noopener noreferrer">
+            <S.YoutubeLink
+              href="https://www.youtube.com/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
               <span className="a11y-hidden">유튜브</span>
             </S.YoutubeLink>
           </li>
