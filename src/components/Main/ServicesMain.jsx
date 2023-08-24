cbbe94de1bf727e88787a5826764a89ef08a5c87
hyeonbin jeong <duafuuu@gmail.com>
2023-08-25 00:44:28 +0900

    add: Services 페이지 grid 추가

--- src/components/Main/ServicesMain.jsx
+++ src/components/Main/ServicesMain.jsx
@@ -1,10 +1,149 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import * as S from './ServicesMain.style';
+import service01 from '../../assets/service01.jpg';
+import service02 from '../../assets/service02.jpg';
+import service03 from '../../assets/service03.jpg';
+import service04 from '../../assets/service04.jpg';
+import service05 from '../../assets/service05.jpg';
+import service06 from '../../assets/service06.jpg';
+import service07 from '../../assets/service07.jpg';
+import service08 from '../../assets/service08.jpg';
+import service09 from '../../assets/service09.jpg';
+import service from '../../assets/service.png';
 
 const ServicesMain = () => {
   return (
-    <main>
-      <section>그리드</section>
-    </main>
+    <S.CustomMain>
+      <S.Article>
+        <S.H2>
+          <img src={service} alt="서비스 아이콘" />
+          <strong>Services</strong>
+          <span>지구를 지키는 마음으로, 한 걸음 더 나아갑니다.</span>
+        </S.H2>
+        <S.Section>
+          <ul>
+            <li>
+              <Link>
+                <img src={service01} alt="서비스01" />
+                <div>
+                  <h3>서비스</h3>
+                  <span>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda ratione,
+                    quos architecto sapiente ex iste quod harum dolorum, omnis voluptates rerum
+                    cumque! Quisquam natus quasi in autem porro, id sunt.
+                  </span>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <img src={service02} alt="서비스02" />
+                <div>
+                  <h3>서비스</h3>
+                  <span>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda ratione,
+                    quos architecto sapiente ex iste quod harum dolorum, omnis voluptates rerum
+                    cumque! Quisquam natus quasi in autem porro, id sunt.
+                  </span>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <img src={service03} alt="서비스03" />
+                <div>
+                  <h3>서비스</h3>
+                  <span>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda ratione,
+                    quos architecto sapiente ex iste quod harum dolorum, omnis voluptates rerum
+                    cumque! Quisquam natus quasi in autem porro, id sunt.
+                  </span>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <img src={service04} alt="서비스04" />
+                <div>
+                  <h3>서비스</h3>
+                  <span>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda ratione,
+                    quos architecto sapiente ex iste quod harum dolorum, omnis voluptates rerum
+                    cumque! Quisquam natus quasi in autem porro, id sunt.
+                  </span>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <img src={service05} alt="서비스05" />
+                <div>
+                  <h3>서비스</h3>
+                  <span>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda ratione,
+                    quos architecto sapiente ex iste quod harum dolorum, omnis voluptates rerum
+                    cumque! Quisquam natus quasi in autem porro, id sunt.
+                  </span>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <img src={service06} alt="서비스06" />
+                <div>
+                  <h3>서비스</h3>
+                  <span>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda ratione,
+                    quos architecto sapiente ex iste quod harum dolorum, omnis voluptates rerum
+                    cumque! Quisquam natus quasi in autem porro, id sunt.
+                  </span>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <img src={service07} alt="서비스07" />
+                <div>
+                  <h3>서비스</h3>
+                  <span>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda ratione,
+                    quos architecto sapiente ex iste quod harum dolorum, omnis voluptates rerum
+                    cumque! Quisquam natus quasi in autem porro, id sunt.
+                  </span>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <img src={service08} alt="서비스08" />
+                <div>
+                  <h3>서비스</h3>
+                  <span>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda ratione,
+                    quos architecto sapiente ex iste quod harum dolorum, omnis voluptates rerum
+                    cumque! Quisquam natus quasi in autem porro, id sunt.
+                  </span>
+                </div>
+              </Link>
+            </li>
+            <li>
+              <Link>
+                <img src={service09} alt="서비스09" />
+                <div>
+                  <h3>서비스</h3>
+                  <span>
+                    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Assumenda ratione,
+                    quos architecto sapiente ex iste quod harum dolorum, omnis voluptates rerum
+                    cumque! Quisquam natus quasi in autem porro, id sunt.
+                  </span>
+                </div>
+              </Link>
+            </li>
+          </ul>
+        </S.Section>
+      </S.Article>
+    </S.CustomMain>
   );
 };
 
---
+++ src/components/Main/ServicesMain.style.jsx
@@ -0,0 +1,78 @@
+import styled from 'styled-components';
+import { Main } from './../common/Layout/MainLayout';
+
+export const CustomMain = styled(Main)`
+  margin: 30px auto;
+  padding: 0px;
+`;
+
+export const Article = styled.article`
+  padding: 30px;
+`;
+
+export const H2 = styled.h2`
+  img {
+    width: 45px;
+    height: 45px;
+
+    @media screen and (min-width: 375px) and (max-width: 1000px) {
+      width: 35px;
+      height: 35px;
+    }
+  }
+
+  strong {
+    font-size: 40px;
+    line-height: 40px;
+    padding-left: 15px;
+
+    @media screen and (min-width: 375px) and (max-width: 1000px) {
+      font-size: 30px;
+      line-height: 30px;
+      padding-left: 10px;
+    }
+  }
+
+  span {
+    display: block;
+    margin: 30px 0;
+    font-size: 25px;
+
+    @media screen and (min-width: 375px) and (max-width: 1000px) {
+      font-size: 20px;
+    }
+  }
+`;
+
+export const Section = styled.section`
+  padding: 40px 0px;
+  ul {
+    display: grid;
+    /* grid-template-columns: repeat(4, 288px); */
+    grid-template-columns: repeat(auto-fill, minmax(288px, 1fr));
+    gap: 50px;
+    margin: auto;
+  }
+
+  li {
+    border-radius: 8px;
+    overflow: hidden;
+    box-shadow: 3px 5px 10px rgba(0, 0, 0, 0.1);
+    background: #fff;
+  }
+
+  img {
+    width: 100%;
+    aspect-ratio: 288/250;
+    object-fit: cover;
+    vertical-align: top;
+  }
+
+  div {
+    padding: 20px;
+
+    h3 {
+      margin-bottom: 10px;
+    }
+  }
+`;
