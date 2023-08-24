d2305c133078cbb242794641ff1a2c93e1d8e8e8
hyeonbin jeong <duafuuu@gmail.com>
2023-08-25 00:43:42 +0900

    add: 스크롤 토글 기능

---
+++ src/components/common/Button/ScrollTopButton.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import upArrow from '../../../assets/upArrow.png';
+
+const ScrollTopButton = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  const toggleVisible = () => {
+    if (window.scrollY > 500) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+
+  const scrollTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  };
+
+  window.addEventListener('scroll', toggleVisible);
+
+  return (
+    <Button onClick={scrollTop} show={isVisible}>
+      <img src={upArrow} alt="스크롤 위로 올리는 화살표 아이콘" />
+    </Button>
+  );
+};
+
+export default ScrollTopButton;
+
+const Button = styled.button`
+  display: ${({ show }) => (show ? 'block' : 'none')};
+  position: fixed;
+  bottom: 80px;
+  right: 30px;
+  z-index: 10;
+
+  img {
+    width: 55px;
+    height: 55px;
+  }
+
+  @media screen and (min-width: 375px) and (max-width: 1000px) {
+    bottom: 60px;
+    right: 30px;
+
+    img {
+      width: 45px;
+      height: 45px;
+    }
+  }
+`;
