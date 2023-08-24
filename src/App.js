0e211c8387e73eeefdb20498a0521640fa8e6213
hyeonbin jeong <duafuuu@gmail.com>
2023-08-25 00:43:08 +0900

    add: 스크롤버튼 생성

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { ThemeProvider } from 'styled-components';
 import theme from './styles/Theme';
 import GlobalStyle from './styles/GlobalStyle';
 import Router from './routes/Router';
+import ScrollTopButton from './components/common/Button/ScrollTopButton';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <GlobalStyle />
       <Router />
+      <ScrollTopButton />
     </ThemeProvider>
   );
 }
