42e4656e64bf048d82fa00bcf685a367093e6b7c
hyeonbin jeong <duafuuu@gmail.com>
2023-09-06 01:16:49 +0900

    add: Map, MapMarker 관련 코드 추가

--- src/components/Main/ContactMain.jsx
+++ src/components/Main/ContactMain.jsx
@@ -2,8 +2,15 @@ import React from 'react';
 import * as S from './ContactMain.style';
 import contact from '../../assets/contact.png';
 import MainLayout from '../common/Layout/MainLayout';
+import { Map } from 'react-kakao-maps-sdk';
+import { MapMarker } from 'react-kakao-maps-sdk';
 
 const ContactMain = () => {
+  const center = {
+    lat: 33.450701,
+    lng: 126.570667,
+  };
+
   return (
     <S.CustomMain>
       <MainLayout
@@ -14,6 +21,18 @@ const ContactMain = () => {
       ></MainLayout>
       <S.Section1>
         <h2 className="a11y-hidden">찾아오시는 길 Map 지도</h2>
+        <Map // 지도 표시
+          center={center}
+          style={{
+            // 지도의 크기
+            width: '100%',
+            height: 'calc(100vh - 450px)',
+            marginTop: '-20px',
+          }}
+          level={3} // 지도의 확대 레벨
+        >
+          <MapMarker position={center} />
+        </Map>
       </S.Section1>
       <S.Section2>
         <h3 className="a11y-hidden">회사 정보 카드</h3>
