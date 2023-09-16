d926307c6380e34a442967de851020adf132784a
hyeonbin jeong <duafuuu@gmail.com>
2023-09-17 01:51:19 +0900

    add: Table 컴포넌트 구조화

---
+++ src/components/common/Table/TableColumn.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const TableColumn = ({ children }) => {
+  return <td>{children}</td>;
+};
+
+export default TableColumn;
---
+++ src/components/common/Table/TableWrap.jsx
@@ -0,0 +1,53 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const TableWrap = ({ headersName, children }) => {
+  return (
+    <Table>
+      <Thead>
+        <tr>
+          {headersName.map((item, index) => {
+            return <th key={index}>{item}</th>;
+          })}
+        </tr>
+      </Thead>
+      <Tbody>{children}</Tbody>
+    </Table>
+  );
+};
+
+export default TableWrap;
+
+const Table = styled.table`
+  width: 100%;
+  text-align: center;
+  border-spacing: 0;
+  margin-bottom: 30px;
+`;
+
+const Thead = styled.thead`
+  tr {
+    background-color: ${({ theme }) => theme.colors.primary};
+    color: ${({ theme }) => theme.colors.white};
+
+    th {
+      padding: 15px 10px;
+      font-size: 17px;
+    }
+  }
+`;
+
+const Tbody = styled.tbody`
+  tr {
+    &:hover {
+      background-color: ${({ theme }) => theme.colors.gray};
+      cursor: pointer;
+    }
+
+    td {
+      font-size: 16px;
+      padding: 18px;
+      border-bottom: ${({ theme }) => `1px dotted ${theme.colors.gray}`};
+    }
+  }
+`;
