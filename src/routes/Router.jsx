2f602ff59e1755cbd2d1f960f7663a1dca18b4c0
hyeonbin jeong <duafuuu@gmail.com>
2023-09-19 19:44:05 +0900

    add: 라우터 및 컴포넌트 추가

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from '../pages/HomePage/Home';
 import About from '../pages/AboutPage/About';
+import Ceo from '../pages/AboutPage/Ceo';
 import Services from '../pages/ServicesPage/Services';
+import ServiceDetail from '../pages/ServicesPage/ServiceDetail';
 import Contact from '../pages/ContactPage/Contact';
 import Notice from '../pages/NoticePage/Notice';
 import Post from '../pages/NoticePage/Post';
@@ -14,7 +16,9 @@ const Router = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
+        <Route path="/about/ceo" element={<Ceo />} />
         <Route path="/services" element={<Services />} />
+        <Route path="/services/detail" element={<ServiceDetail />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/notice" element={<Notice />} />
         <Route path="/notice/post" element={<Post />} />
