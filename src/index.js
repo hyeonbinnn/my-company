365bf178acb238026b03a6ac02d1b648f172364a
hyeonbin jeong <duafuuu@gmail.com>
2024-02-16 20:07:06 +0900

    add: QueryClientProvider를 위치 적용

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import { RecoilRoot } from 'recoil';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
+
+const queryClient = new QueryClient();
+
 root.render(
   <RecoilRoot>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </RecoilRoot>
 );
