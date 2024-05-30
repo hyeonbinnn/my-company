8cbede10751f760712e660e10d6d1677ce211a24
hyeonbin jeong <duafuuu@gmail.com>
2024-05-30 15:31:06 +0900

    refactor: 코드 스플리팅으로 초기 로딩 시간 단축

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,27 +1,33 @@
+import { lazy, Suspense } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from '../pages/HomePage/Home';
-import About from '../pages/AboutPage/About';
-import Service from '../pages/ServicePage/Service';
-import Contact from '../pages/ContactPage/Contact';
-import Notice from '../pages/NoticePage/Notice';
-import UploadPost from '../pages/NoticePage/UploadPost';
-import Error404 from '../pages/ErrorPage/Error404';
-import PostDetail from '../pages/NoticePage/PostDetail';
+
+import Loading from '../components/Loading/Loading';
+
+const Home = lazy(() => import('../pages/HomePage/Home'));
+const About = lazy(() => import('../pages/AboutPage/About'));
+const Service = lazy(() => import('../pages/ServicePage/Service'));
+const Contact = lazy(() => import('../pages/ContactPage/Contact'));
+const Notice = lazy(() => import('../pages/NoticePage/Notice'));
+const UploadPost = lazy(() => import('../pages/NoticePage/UploadPost'));
+const Error404 = lazy(() => import('../pages/ErrorPage/Error404'));
+const PostDetail = lazy(() => import('../pages/NoticePage/PostDetail'));
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/service" element={<Service />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/notice" element={<Notice />} />
-        <Route path="/notice/upload" element={<UploadPost />} />
-        <Route path="/notice/:id" element={<PostDetail />} />
-        <Route path="*" element={<Error404 />} />
-        <Route path="/error" element={<Error404 />} />
-      </Routes>
+      <Suspense fallback={<Loading />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/service" element={<Service />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/notice" element={<Notice />} />
+          <Route path="/notice/upload" element={<UploadPost />} />
+          <Route path="/notice/:id" element={<PostDetail />} />
+          <Route path="*" element={<Error404 />} />
+          <Route path="/error" element={<Error404 />} />
+        </Routes>
+      </Suspense>
     </BrowserRouter>
   );
 };
